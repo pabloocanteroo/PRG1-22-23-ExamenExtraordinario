2bcb36a7d59f2a4b47db80cb9cd19257990543a0
pabloocanteroo <114483234+pabloocanteroo@users.noreply.github.com>
2023-07-03 09:47:57 +0200

    Intentando que se imprima el texto en su lugar
    Tratando de que no vaya un lugar mas alante el texto

--- canteroPablo/hojaCalculo.java
+++ canteroPablo/hojaCalculo.java
@@ -8,11 +8,10 @@ public class hojaCalculo {
     int [][] hojaCalculo = new int [filas][columnas];
     int [] posicionJugador = {0,0};
     boolean terminar = false;
-    do{
-
-    System.out.println("+------------------------------------------------------------------------+");
+    String texto;
+     System.out.println("+------------------------------------------------------------------------+");
     System.out.println("|  |   A  |   B  |   C  |   D  |   E  |   F  |   G  |   H  |   I  |   J  |");
-    System.out.println("+-------------------------------------------------------------------------+");
+    System.out.println("+------------------------------------------------------------------------+");
     for(int i=0;i<hojaCalculo.length;i++){
         if(i<10)
         {System.out.print("| "+ i +"|");
@@ -21,38 +20,96 @@ public class hojaCalculo {
         }
         
         for(int j=0;j<hojaCalculo[i].length;j++){
+
             if(posicionJugador[0]==i&&posicionJugador[1]==j){
                 System.out.print("[____]");
+                
+
             }else{
                 System.out.print("      ");
+                hojaCalculo[i][j]=0;
             }
-            hojaCalculo[i][j]=0;
             System.out.print("|");
+            
         }
         System.out.println();
     }
-    System.out.println("+-------------------------------------------------------------------------+");
+    System.out.println("+------------------------------------------------------------------------+");
     System.out.println("|   | Y: "+posicionJugador[0]+" | X: "+posicionJugador[1]+" |");
-    System.out.println("+-------------------------------------------------------------------------+");
+    System.out.println("+------------------------------------------------------------------------+");
     System.out.println("WASD para moverse o f para salir");
     System.out.println("Introduce una tecla");
     inputUsuario = entrada.nextLine().charAt(0);
-            switch (inputUsuario) {
-                case 's', 'S', '8':
-                    posicionJugador[0] = posicionJugador[0] + 1;
-                    break;
-                case 'w', 'W', '2':
-                    posicionJugador[0] = posicionJugador[0] - 1;
-                    break;
-                case 'a', 'A', '4':
-                    posicionJugador[1] = posicionJugador[1] - 1;
-                    break;
-                case 'd', 'D', '6':
-                    posicionJugador[1] = posicionJugador[1] + 1;
-                    break;
-                case 'f', 'F':
-                    terminar = true;
+            terminar = moverJugador(inputUsuario, posicionJugador, terminar);
+            limits(posicionJugador);
+    
+    do{
+        System.out.println("¿Que quieres escribir?");
+        texto = entrada.nextLine();
+
+    System.out.println("+------------------------------------------------------------------------+");
+    System.out.println("|  |   A  |   B  |   C  |   D  |   E  |   F  |   G  |   H  |   I  |   J  |");
+    System.out.println("+------------------------------------------------------------------------+");
+    for(int i=0;i<hojaCalculo.length;i++){
+        if(i<10)
+        {System.out.print("| "+ i +"|");
+        }else{
+            System.out.print("|"+ i +"|");
+        }
+        
+        for(int j=0;j<hojaCalculo[i].length;j++){
+
+            if(posicionJugador[0]==i&&posicionJugador[1]==j){
+                System.out.print("[____]");
+                hojaCalculo[i][j]=3;
+
+            }else{
+                System.out.print("      ");
+                hojaCalculo[i][j]=0;
             }
+            System.out.print("|");
+            if(hojaCalculo[i][j]==3){
+                System.out.print("texto");
+            }
+        }
+        System.out.println();
+    }
+    
+    
+    System.out.println("+------------------------------------------------------------------------+");
+    System.out.println("|   | Y: "+posicionJugador[0]+" | X: "+posicionJugador[1]+" |");
+    System.out.println("+------------------------------------------------------------------------+");
+    System.out.println("WASD para moverse o f para salir");
+    System.out.println("Introduce una tecla");
+    inputUsuario = entrada.nextLine().charAt(0);
+        terminar = moverJugador(inputUsuario, posicionJugador, terminar);
+        limits(posicionJugador);
+    
+
+    }while(terminar==false);
+    }
+
+    private static boolean moverJugador(char inputUsuario, int[] posicionJugador, boolean terminar) {
+        switch (inputUsuario) {
+            case 's', 'S', '8':
+                posicionJugador[0] = posicionJugador[0] + 1;
+                break;
+            case 'w', 'W', '2':
+                posicionJugador[0] = posicionJugador[0] - 1;
+                break;
+            case 'a', 'A', '4':
+                posicionJugador[1] = posicionJugador[1] - 1;
+                break;
+            case 'd', 'D', '6':
+                posicionJugador[1] = posicionJugador[1] + 1;
+                break;
+            case 'f', 'F':
+                terminar = true;
+        }
+        return terminar;
+    }
+
+    private static void limits(int[] posicionJugador) {
         if(posicionJugador[0]<0){
             posicionJugador[0]=0;
         }else if(posicionJugador[0]>14){
@@ -62,8 +119,5 @@ public class hojaCalculo {
         }else if(posicionJugador[1]>9){
             posicionJugador[1]=9;
         }
-    System.out.println("¿Que quieres escribir?");
-    
-    }while(terminar==false);
     }
 }
